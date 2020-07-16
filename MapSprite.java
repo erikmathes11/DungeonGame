2b797408a0b7c16144b9d7e2c5236e63f0fa6f6f
Erik Mathes <erik.mathes20@d155.org>
2020-07-15 22:12:15 -0500

    Still working on map UI.

--- MapSprite.java
+++ MapSprite.java
@@ -29,19 +29,23 @@ public class MapSprite extends UISprite
         }
         catch (Exception e)
         {
-            
+
         }
-        backgroundImage = robot.createScreenCapture(new Rectangle((int) x, (int) y, 0, map.getHeight()));
+        backgroundImage = robot.createScreenCapture(new Rectangle((int) x, (int) y, 0, uISprite.getIconHeight()));
         backgroundImage2 = robot.createScreenCapture(new Rectangle());
-        
+
     }
-    
+
     public void drawUIElement (Graphics2D g2D, GamePanel panel1)
     {
         int xEndPoint = 0;
-        int yEndPoint = 0;
-        g2D.drawImage(map, t, panel1);
+        uISprite.paintIcon(panel1, g2D, (int) x, (int) y);
         g2D.drawImage(backgroundImage, t, panel1);
+        for (int i = (int) x; robot.getPixelColor(i, 0) == Color.WHITE; i++)
+        {
+            xEndPoint = i - 1;
+        }
+        
         for (int i = 0; i < quadrants.length; i++)
         {
             for (int j = 0; j < quadrants[i].length; j++)
@@ -49,19 +53,8 @@ public class MapSprite extends UISprite
                 quadrants[i][j].drawQuadrantOnMap(g2D, panel1);
             }
         }
-        
-        for (int i = (int) x; robot.getPixelColor(i, 0) == Color.WHITE; i++)
-        {
-            xEndPoint = i - 1;
-        }
-        for (int i = (int) y; robot.getPixelColor(0, i) == Color.WHITE; i++)
-        {
-            yEndPoint = i - 1;
-        }
-        
+
         System.out.println("xEndPoint: " + xEndPoint);
-        System.out.println("yEndPoint: " + yEndPoint);
-        
-        
+
     }
 }
